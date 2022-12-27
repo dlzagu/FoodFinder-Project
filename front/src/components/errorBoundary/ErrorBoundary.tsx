b9fe668d08540afee2da6343a903a318afe8d6af
dlzagu <eodnsdlekd@naver.com>
2022-12-27 22:09:04 +0900

    fix : fixed exception handling

--- front/src/components/errorBoundary/ErrorBoundary.tsx
+++ front/src/components/errorBoundary/ErrorBoundary.tsx
@@ -1,4 +1,8 @@
-import { Component, PropsWithChildren } from 'react';
+import { Component, PropsWithChildren, ErrorInfo } from 'react';
+import { AxiosError } from 'axios';
+import Storage from '../../storage/storage';
+
+class CustomAxiosError extends AxiosError<{ message: string }> {}
 
 export interface FallbackProps {
   error: Error;
@@ -12,10 +16,12 @@ interface ErrorBoundaryProps {
 
 interface ErrorBoundaryState {
   error: Error | null;
+  errorCase: string | null;
 }
 
 const initialState: ErrorBoundaryState = {
   error: null,
+  errorCase: null,
 };
 
 class ErrorBoundary extends Component<
@@ -24,24 +30,77 @@ class ErrorBoundary extends Component<
 > {
   state: ErrorBoundaryState = {
     error: null,
+    errorCase: null,
   };
 
   resetErrorBoundary = () => {
-    this.props.onReset?.();
     this.setState(initialState);
   };
 
   static getDerivedStateFromError(error: Error): ErrorBoundaryState {
-    return { error };
+    if (!(error instanceof AxiosError)) {
+      return { error, errorCase: null };
+    }
+
+    if (error.response?.status === 401) {
+      return {
+        error,
+        errorCase: 'unauthorized',
+      };
+    }
+
+    if (error.response?.config.method === 'get') {
+      console.log('geterror');
+      return {
+        error,
+        errorCase: 'get',
+      };
+    }
+
+    return { error, errorCase: null };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    const { error: errorState, errorCase } = this.state;
+
+    if (errorCase === 'unauthorized') {
+      alert('다시 로그인해주세요.');
+      return;
+    }
+    if (errorCase === 'get') {
+      if (errorState instanceof CustomAxiosError) {
+        const errorMessage =
+          errorState.response?.data.message ||
+          '알 수 없는 에러가 발생했습니다.';
+
+        alert(errorMessage);
+
+        return;
+      }
+
+      alert('알 수 없는 에러가 발생했습니다.');
+
+      return;
+    }
+
+    if (errorCase === null) {
+      alert('알 수 없는 에러가 발생했습니다.');
+    }
   }
 
   render() {
     const { fallback: FallbackComponent } = this.props;
+    const { error, errorCase } = this.state;
 
-    if (this.state.error) {
+    if (errorCase === 'unauthorized') {
+      Storage.clearToken();
+      window.location.replace('/login');
+    }
+
+    if (error) {
       return (
         <FallbackComponent
-          error={this.state.error}
+          error={error}
           resetErrorBoundary={this.resetErrorBoundary}
         />
       );
--- front/src/util/customAxios.ts
+++ front/src/util/customAxios.ts
@@ -30,13 +30,7 @@ customAxios.interceptors.response.use(
       const { statusText, data } = error.response;
       const { message } = data;
       if (statusText === 'Unauthorized') {
-        if (
-          message ===
-          '유효기간이 만료된 리프레시 토큰입니다. 유저를 로그인 화면으로 보내주세요'
-        ) {
-          Storage.clearToken();
-          window.location.replace('/login');
-        } else if (message === '유효기간이 만료된 토큰입니다.') {
+        if (message === '유효기간이 만료된 토큰입니다.') {
           (async () => {
             const res = await customAxios.put(
               'auth/validation/refresh-token',
@@ -56,7 +50,7 @@ customAxios.interceptors.response.use(
       } else if (
         message ===
           '서버에 저장되지 않은 리프레시 토큰입니다. 로그인 화면으로' ||
-        message === '손상된 토큰입니다.'
+        message === '"손상된 토큰입니다. 유저를 로그인 화면으로 보내주세요"'
       ) {
         Storage.clearToken();
         window.location.replace('/login');
